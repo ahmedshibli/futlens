511b3cf556b006dca46fe5c656f503806b5f7b46
ahmedshibli <157249430+ahmedshibli@users.noreply.github.com>
2024-08-29 00:13:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,72 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+
+
+     // Fixed Navbar
+     $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.fixed-top .container').addClass('shadow-sm').css('max-width', '100%');
+        } else {
+            $('.fixed-top .container').removeClass('shadow-sm').css('max-width', '85%');
+        }
+    });
+
+
+   
+
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:3
+            }
+        }
+    });
+
+    
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 0, 'easeInOutExpo');
+        return false;
+    }); 
+
+})(jQuery);
+
